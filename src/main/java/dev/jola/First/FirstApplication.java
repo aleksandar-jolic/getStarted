12068daae426f67cdc05e2bc32ab3d3d13b9b31d
aleksandar-jolic <coajolic@gmail.com>
2023-02-18 12:52:53 +0100

    1.change

--- src/main/java/dev/jola/First/FirstApplication.java
+++ src/main/java/dev/jola/First/FirstApplication.java
@@ -2,12 +2,18 @@ package dev.jola.First;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.util.BsonUtils;
 
 @SpringBootApplication
 public class FirstApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
+
+		System.out.println("Novina");
+
 	}
+
+
 
 }
